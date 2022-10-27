52baa4e24fa764a46341442f2559b04e2cae800c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-27 19:37:11 +0600

    Initiating!

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import webdriver
 from bs4 import BeautifulSoup
+import requests
+
 
 link = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.64481581640625%2C%22east%22%3A-122.22184218359375%2C%22south%22%3A37.69763921595251%2C%22north%22%3A37.852862216052614%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 
-soup = BeautifulSoup(link, "html.parser")
-print(soup.title)
+response = requests.get(url=link).text
+
+soup = BeautifulSoup(response, "html.parser")
+print(soup.prettify())
